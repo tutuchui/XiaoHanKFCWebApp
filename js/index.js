794fe66d32e9f3fa954bbd9c3cb78a72f8eb2ac2
Yutong Wang <598402463@qq.com>
2021-07-07 16:58:11 +0800

    upload page and display page

--- js/index.js
+++ js/index.js
@@ -1,49 +1,58 @@
-var hamburger = new Object();
-hamburger.title = "汉堡";
-hamburger.introduction = "汉堡的介绍";
-hamburger.id = "hamburger";
-hamburger.imageUrl = "images/hamburger.jpeg"
+var mainMealProduct = [];
+var dessertProduct = [];
+var snakesProduct = [];
 
-var fries = new Object();
-fries.title = "薯条";
-fries.introduction = "薯条的介绍";
-fries.id = "fries";
-fries.imageUrl = "images/fries.jpeg"
-
-var mainProduct =[hamburger, fries];
-
-var iceCream = new Object();
-iceCream.title = "冰淇淋";
-iceCream.introduction = "冰淇淋的介绍";
-iceCream.id = "icecream";
-iceCream.imageUrl = "images/icecream.jpeg"
-
-var dessertProduct = [iceCream];
+var productCountMap = new Map();
 $(document).ready(function () {
+    $.ajax({
+        url: "http://localhost:8080/getAllProducts",
+        type: "GET",
+        dataType: "json",
+        header:{
+            'Access-Control-Allow-Origin': "http://localhost:8080"
+        },
+        success:function (data) {
+            prepareProducts(data)
+        },
+        async:false
+    })
+
     displayProduct('main');
 })
+function prepareProducts(productList) {
+    for(i = 0; i < productList.length; i++){
+        productCountMap.set('product'+productList[i].id, 0);
+        if(productList[i].category === "mainMeal"){
+            mainMealProduct.push(productList[i]);
+        }else if(productList[i].category === "dessert"){
+            dessertProduct.push(productList[i]);
+        }
+    }
+
+}
 function addProduct(ele){
     var productId = $(ele).attr('value') + '-count';
     var productCount = $("#" + productId);
-    curCount = parseInt(productCount.html()) + 1;
-    productCount.html(curCount);
+    productCountMap.set($(ele).attr('value'), productCountMap.get($(ele).attr('value')) + 1);
+    productCount.html(productCountMap.get($(ele).attr('value')));
 }
 
 function minusProduct(ele){
     var productId = $(ele).attr('value') + '-count';
     var productCount = $("#" + productId);
-    curCount = parseInt(productCount.html()) - 1;
+    var curCount = productCountMap.get($(ele).attr('value')) - 1;
     if(curCount < 0){
         curCount = 0;
     }
-    productCount.html(curCount);
+    productCountMap.set($(ele).attr('value'), curCount);
+    productCount.html(productCountMap.get($(ele).attr('value')));
 }
 
 
 function displayProduct(category) {
-    var product = mainProduct;
+    var product = mainMealProduct;
     if(category === 'main'){
-         product = mainProduct;
+         product = mainMealProduct;
     }else if(category === 'dessert'){
         product = dessertProduct;
     }
@@ -53,15 +62,14 @@ function displayProduct(category) {
             '<div class="col card me-3" style="width: 18rem;">' +
                 '<img src="' + product[i].imageUrl + '" class="card-img-top hfc-card-image">' +
                 '<div class="card-body">' +
-                    '<h5 class="card-title">' + product[i].title + '</h5>' +
+                    '<h5 class="card-title">' + product[i].name + '</h5>' +
                     '<p class="card-text">' + product[i].introduction + '</p>' +
-                    '<button class="btn btn-danger" onclick="addProduct(this)" value=' + product[i].id + '><i class="bi bi-plus"></i></button>' +
-                    '&nbsp;<span id=' + product[i].id + '-count' + '>0</span>&nbsp;' +
-                    '<button class="btn btn-secondary" onclick="minusProduct(this)" value=' + product[i].id + '><i class="bi bi-dash"></i></button>' +
+                    '<button class="btn btn-danger" onclick="addProduct(this)" value=' + 'product' +product[i].id + '><i class="bi bi-plus"></i></button>' +
+                    '&nbsp;<span id=' + 'product' + product[i].id + '-count' + '>'+ productCountMap.get('product' + product[i].id) + '</span>&nbsp;' +
+                    '<button class="btn btn-secondary" onclick="minusProduct(this)" value=' + 'product'+product[i].id + '><i class="bi bi-dash"></i></button>' +
                 '</div>' +
             '</div>'
         $(".main-product-container").append(productHtml);
     }
-
 
 }
---
+++ js/management.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    $("#image-file").change(function () {
+        readURL(this);
+    })
+})
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#img-upload').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}P
+
+function uploadProduct(){
+    var isValidInput = true;
+    var productName = $("#product-name").val();
+    var productPrice = $("#product-price").val();
+    var productCategory = $("#category").val();
+    var productIntroduction = $("#product-introduction").val();
+    var priceRegEx = new RegExp('^\\d+(\\.\\d+)?$');
+    var imageFile = $("#image-file");
+    if(productName === ''){
+        $("#product-name").addClass("is-invalid");
+        isValidInput = false;
+    }
+    if(productIntroduction === ''){
+        $("#product-introduction").addClass("is-invalid");
+        isValidInput = false;
+    }
+    if(!priceRegEx.test(productPrice) || productPrice === ''){
+        $("#product-price").addClass("is-invalid");
+        isValidInput = false;
+    }
+    if(!isValidInput){
+        return;
+    }
+    var formData = new FormData();
+    formData.append('imageFile',imageFile[0].files[0])
+    formData.append("productName", productName);
+    formData.append("productPrice",productPrice);
+    formData.append("productCategory",productCategory);
+    formData.append("productIntroduction", productIntroduction);
+    $.ajax({
+        url:"http://localhost:8080/uploadProduct",
+        type:"POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        header:{
+            'Acess-Control-Allow-Origin':'http://localhost:8080'
+        },
+        success: function (data) {
+            if(data === 'Success'){
+                alert("上传成功")
+            }
+        },
+        error: function (xhr, status, errorMessage) {
+            alert("上传失败")
+        }
+    })
+}
